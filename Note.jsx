4b19b43f157c8bce90f0327f70a428499a9bad28
Vishnuraj Saravanan <76027763+vishnu02-droid@users.noreply.github.com>
2021-06-05 19:27:48 +0530

    Create Note.jsx

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        I have attended the 7 days free bootcamp on Web development with Javascript & React.js conducted by Shape AI
+      </p>
+    </div>
+  );
+}
+
+export default Note;
